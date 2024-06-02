3946d9f19593c22ea92d2d51201f7919d4e10755
wdmzjwzz <2216453885@qq.com>
2024-06-02 16:13:22 +0800

    fix

--- apps/gateway/src/gateway.service.ts
+++ apps/gateway/src/gateway.service.ts
@@ -1,12 +1,21 @@
 
 import { AUTH_SERVICE_GRPC_SERVICE_NAME, AuthServiceGrpcClient } from '@app/grpc';
-import { Metadata } from '@grpc/grpc-js';
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class GatewayService {
+
+  private authServiceClient: AuthServiceGrpcClient;
+  constructor(
+    @Inject(AUTH_SERVICE_GRPC_SERVICE_NAME) private client: ClientGrpc
+  ) { }
+
+  onModuleInit() {
+    this.authServiceClient = this.client.getService<AuthServiceGrpcClient>(AUTH_SERVICE_GRPC_SERVICE_NAME);
+  }
+
   async getHello() {
     const $data = this.authServiceClient.checkToken({
       token: '22222'
@@ -14,10 +23,7 @@ export class GatewayService {
     const { data } = await firstValueFrom($data);
     return 'Hello World GatewayService!' + data.account;
   }
-  private authServiceClient: AuthServiceGrpcClient;
-  constructor(@Inject(AUTH_SERVICE_GRPC_SERVICE_NAME) private client: ClientGrpc) { }
 
-  onModuleInit() {
-    this.authServiceClient = this.client.getService<AuthServiceGrpcClient>(AUTH_SERVICE_GRPC_SERVICE_NAME);
-  }
+
+
 }
